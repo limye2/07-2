8a83139cc05a68c493cf96eb2d96c100e9498c27
limye2 <144992260+limye2@users.noreply.github.com>
2023-10-19 15:21:58 +0900

    Initial Commit

---
+++ 07-2/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  07-2
+//
+//  Created by MacBook Air on 2023/10/19.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    
+    for(i=0;i<5;i++)
+    {
+        int temp = 1;
+        printf("temp = %d\n", temp);
+        temp++;
+    }
+    return 0;
+}
